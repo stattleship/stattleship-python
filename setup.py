2383c60c2ad992dd8dae53a9d4d89eefcd172a7b
Btibert3 <btibert3@gmail.com>
2015-12-10 20:37:06 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='stattlepy',
+    version='0.1dev',
+    packages=['stattlepy',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
