0ff99980f5faf2cf8e7e9857febbc534518370eb
Drew Pinta <dpinta6@gmail.com>
2016-05-08 15:38:13 -0400

    Use relative import

--- stattlepy/__init__.py
+++ stattlepy/__init__.py
@@ -1,5 +1,5 @@
 ### Import Statteleship class upon initiation of the program
-from Stattleship_API import Stattleship
+from .Stattleship_API import Stattleship
 
 from pkg_resources import get_distribution
 
